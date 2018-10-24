a11d704919aa06c5df799c0320f0cea7eb5361d4
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2018-10-24 15:11:27 -0500

    agregando data para cargar la jerarquia de datos

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Colombia - Jerarquia de cuentas',
+    'category': 'Localization',
+    'version': '10.01',
+    'author': 'Odoo LoCo',
+    'license': 'AGPL-3',
+    'maintainer': '',
+    'website': '',
+    'summary': '',
+    'images': [],
+    'depends': [
+        'l10n_co',
+        'account_parent',
+    ],
+    'data': [
+        'data/data_account_type.xml',
+        'data/account.account.template.csv'],
+    'installable': True,
+}
